40a1acf3649461b3ed7918db1ac6b35b7e3eaca6
ArchitHKIS <190902@hkis.edu.hk>
2016-09-10 12:13:53 +0800

    Added Comments

--- src/RandomNumberGenerator.java
+++ src/RandomNumberGenerator.java
@@ -7,7 +7,9 @@ public static void main(String[] args) {
       Random r = new Random();
       Scanner in = new Scanner(System.in);
       ArrayList<Integer> poops = new ArrayList<Integer>(totalPoints);
+      // poops are the random numbers that are generated
       ArrayList<Integer> userinputs = new ArrayList<Integer>(totalPoints);
+      // userinputs are the numbers inputed by the user
 
       for (int i=0; i<totalPoints; i++) {
           int poop = (r.nextInt(10-1)+1);
@@ -16,6 +18,8 @@ public static void main(String[] args) {
       }
 
       for (int poto = 0, correct = 0; poto < 5; poto = poto + correct) {
+    	  //poto is used for the for loop, as long as poto is less that 5 the code won't run. Used for 
+    	 // measuring the user inputs. 
       System.out.println("Enter a number from 1-9");
       for (int i=0; i<4; i++) {
           int s = in.nextInt();	
